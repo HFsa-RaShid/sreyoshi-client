8a14146f51878f8ef6010a04b9f7c01a4abef2c4
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-07-11 14:20:02 +0600

    update

--- app/(withCommonLayout)/shop/page.tsx
+++ app/(withCommonLayout)/shop/page.tsx
@@ -1147,9 +1147,9 @@ function ShopContent() {
   const showSkeleton = isCategoriesLoading || isProductsLoading;
 
   return (
-    <div className="bg-[#FAF9F6] min-h-screen pt-20 md:pt-28 pb-12 px-4 md:px-12 text-[#2C3E35]">
+    <div className="bg-[#FAF9F6] min-h-screen pt-28 pb-12 px-4 md:px-12 text-[#2C3E35]">
       <div className="container mx-auto">
-        <div className="mb-2 md:mb-6 md:pl-2">
+        <div className="md:mb-6 md:pl-2">
           <Breadcrumb />
         </div>
 
