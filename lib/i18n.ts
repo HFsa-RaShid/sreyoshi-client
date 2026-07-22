25fa8fec8fca0257fdc03122caa1dda03bc4eca6
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-07-22 12:37:46 +0600

    update

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,16 +1,17 @@
 // src/lib/i18n.ts
+"use client"; // 👈 এই লাইনটি যোগ না থাকলে সার্ভার সাইড বিল্ড ফেল করবে!
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  resources: {}, // 👈 কোনো JSON ফাইল দেওয়া হলো না!
+  resources: {},
   lng: "en",
   fallbackLng: "en",
   interpolation: {
-    escapeValue: false, // React-এর জন্য
+    escapeValue: false,
   },
-  // Key না পেলে সরাসরি টেক্সটটিই রিটার্ন করবে
-  parseMissingKeyHandler: (key) => key, 
+  parseMissingKeyHandler: (key) => key,
 });
 
 export default i18n;
